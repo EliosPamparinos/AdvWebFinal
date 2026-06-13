0e1de15d14b2f3a8803967184b0ce7ceb151d2c5
Elya Chaalan <ps3email312@gmail.com>
2026-06-14 00:03:16 +0300

    Update Register.jsx

--- AdvWebFinal.Server/advwebfinal.client/src/Pages/Register.jsx
+++ AdvWebFinal.Server/advwebfinal.client/src/Pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import api from '../Services/api';
+import api from '../Services/Api';
 import { useAuth } from '../Context/AuthContext';
 
 const Register = () => {
