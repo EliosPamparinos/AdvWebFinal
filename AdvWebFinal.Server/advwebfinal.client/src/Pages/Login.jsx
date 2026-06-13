ddb47bb6ab6884e1fa89d86bf07289ca545afd77
Elya Chaalan <ps3email312@gmail.com>
2026-06-13 23:56:33 +0300

    Update Login.jsx

--- AdvWebFinal.Server/advwebfinal.client/src/Pages/Login.jsx
+++ AdvWebFinal.Server/advwebfinal.client/src/Pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
-import api from '../services/api';
+import { useAuth } from '../Context/AuthContext';
+import api from '../Services/api';
 
 const Login = () => {
     const [form, setForm] = useState({ email: '', password: '' });
