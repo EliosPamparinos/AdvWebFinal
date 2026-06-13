47df6173f312f1f23b2c277cb6536be7267da5c5
Elya Chaalan <ps3email312@gmail.com>
2026-06-14 00:06:03 +0300

    Update Navbar.jsx

--- AdvWebFinal.Server/advwebfinal.client/src/Components/Navbar.jsx
+++ AdvWebFinal.Server/advwebfinal.client/src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../Context/AuthContext';
 
 const Navbar = () => {
     const { token, logout } = useAuth();
