b782f68915681f813ae00d0e4f573ca7a41959ce
Elya Chaalan <ps3email312@gmail.com>
2026-06-13 23:57:16 +0300

    Update Tasks.jsx

--- AdvWebFinal.Server/advwebfinal.client/src/Pages/Tasks.jsx
+++ AdvWebFinal.Server/advwebfinal.client/src/Pages/Tasks.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
-import { useAuth } from '../context/AuthContext';
-import api from '../services/api';
+import { useAuth } from '../Context/AuthContext';
+import api from '../Services/api';
 
 const Tasks = () => {
     const [tasks, setTasks] = useState([]);
