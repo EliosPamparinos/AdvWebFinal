71a26fc5252686d5e492608d4eedd4e9f568cb34
Elya Chaalan <ps3email312@gmail.com>
2026-06-13 23:55:56 +0300

    Update App.jsx

--- AdvWebFinal.Server/advwebfinal.client/src/App.jsx
+++ AdvWebFinal.Server/advwebfinal.client/src/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Navbar from './components/Navbar';
-import Projects from './pages/Projects';
-import ProtectedRoute from './components/ProtectedRoute';
-import Tasks from './pages/Tasks';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+import Navbar from './Components/Navbar';
+import Projects from './Pages/Projects';
+import ProtectedRoute from './Pomponents/ProtectedRoute';
+import Tasks from './Pages/Tasks';
 
 const App = () => {
     return (
